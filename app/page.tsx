b76a0cd8cb8fc5537eb73949d46449f8a919e92e
Muthee00 <harrymuthee254@gmail.com>
2025-04-19 22:49:11 +0300

    fix prisma2

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
   );
 }
 
-export async function BlogPosts() {
+async function BlogPosts() {
   const data = await getPosts();
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
