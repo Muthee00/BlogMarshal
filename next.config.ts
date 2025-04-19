670f04bda46b361ab3141be9de017af8ffaf4656
Muthee00 <harrymuthee254@gmail.com>
2025-04-19 23:40:59 +0300

    fix prismafg4df

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: isProd,
+  },
   images: {
     remotePatterns: [
       {
